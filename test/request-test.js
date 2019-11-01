09fed066d8b29d9838ee8370453ad8f2880cc355
Mark Tyneway <mark@purse.io>
2019-10-31 23:11:47 -0700

    test: coverage

---
+++ test/request-test.js
@@ -0,0 +1,111 @@
+/**
+ *
+ */
+
+'use strict';
+
+const {Server} = require('../lib/bweb');
+const assert = require('bsert');
+const {
+  Client,
+  resDeepEqual,
+  resHeaderDeepEqual
+} = require('./utils/common');
+const Request = require('../lib/request');
+
+const port = 9219;
+
+let client, server;
+let seen = false;
+
+describe('Request', function() {
+  before(() => {
+    server = new Server({
+      port: port,
+    });
+
+    client = new Client();
+
+    server.use(server.router());
+  })
+
+  beforeEach(async () => {
+    server.on('error', async (err) => {
+      assert.fail(err);
+    });
+
+    await server.open();
+  });
+
+  afterEach(async () => {
+    await server.close();
+    seen = false;
+  });
+
+  it('should construct with default values', async () => {
+    server.get('/', async (hreq, hres) => {
+      const req = new Request(hreq, hres, hreq.url);
+
+      assert.deepEqual(req.req, hreq);
+      assert.deepEqual(req.res, hres);
+      assert.deepEqual(req.socket, hreq.socket);
+      assert.deepEqual(req.type, 'bin');
+      assert.deepEqual(req.url, '/');
+      assert.deepEqual(req.pathname, '/');
+      assert.deepEqual(req.path, []);
+      assert.deepEqual(req.trailing, false);
+      assert.deepEqual(req.username, null);
+      assert.deepEqual(req.query, Object.create(null));
+      assert.deepEqual(req.params, Object.create(null));
+      assert.deepEqual(req.body, Object.create(null));
+      assert.deepEqual(req.cookies, Object.create(null));
+      assert.deepEqual(req.hasBody, false);
+      assert.deepEqual(req.readable, true);
+      assert.deepEqual(req.writable, false);
+      assert.deepEqual(req.admin, false);
+      assert.deepEqual(req.wallet, null);
+
+      seen = true;
+      return hres.end();
+    });
+
+    const res = await client.request({
+      hostname: 'localhost',
+      port: port,
+      headers: {},
+      method: 'GET',
+      path: '/'
+    });
+
+    assert.equal(seen, true);
+  });
+
+  it('should navigate', async () => {
+    server.post('/foo', async (hreq, hres) => {
+      const req = new Request(hreq, hres, hreq.url);
+      req.navigate('/?foo=bar');
+
+      assert.deepEqual(req.url, '/?foo=bar');
+      assert.deepEqual(req.pathname, '/');
+      assert.deepEqual(req.path, []);
+
+      // Must have null prototype.
+      const query = Object.create(null);
+      query.foo = 'bar';
+
+      assert.deepEqual(req.query, query);
+
+      seen = true;
+    });
+
+    const res = await client.request({
+      hostname: 'localhost',
+      port: port,
+      headers: {},
+      method: 'POST',
+      path: '/foo'
+    });
+
+    assert(seen);
+  });
+});
